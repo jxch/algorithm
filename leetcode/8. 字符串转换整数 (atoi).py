62533b30f382c853966d68b6012c6a295c2263a3
jxch <15553598935@163.com>
2022-03-15 10:17:07 +0800

    8. 字符串转换整数 (atoi)

---
+++ leetcode/8. 字符串转换整数 (atoi).py
@@ -0,0 +1,5 @@
+# https://leetcode-cn.com/problems/string-to-integer-atoi/
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
